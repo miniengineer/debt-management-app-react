7f78802f3272f97f5dfb76767a87909ad5dd040b
Minira Samadova <samadova.minira@gmail.com>
2019-08-25 10:32:18 +0900

    added test database

--- src/components/App.js
+++ src/components/App.js
@@ -27,21 +27,28 @@ export default class App extends Component {
         totalDebt: this.props.initialDebt
       });
     });
+    axios.get("https://rocky-ocean-72205.herokuapp.com/transactions").then((response) => {
+      this.setState({
+        transactionList: response.data.reverse()
+      });
+    });
   }
 
   //getting inputted transaction from child component
   onFormSubmitted = newTransaction => {
+    //send new transaction to db
     axios.post("https://rocky-ocean-72205.herokuapp.com/transactions", {
       "date": newTransaction.date.toISOString(),
       "amount": newTransaction.amount,
       "currency": this.props.initialCurrency
-    });
-    var newTransactionList = [...this.state.transactionList, newTransaction];
-    newTransactionList.sort((a, b) => a.date.getTime() - b.date.getTime());
+    }).then((response) => {
+      //get updated transaction list from db
+      let newTransactionList = [response.data, ...this.state.transactionList];
 
-    this.setState({
-      transactionList: newTransactionList,
-      totalDebt: this.state.totalDebt - newTransaction.amount
+      this.setState({
+        transactionList: newTransactionList,
+        totalDebt: this.state.totalDebt - newTransaction.amount
+      });
     });
   };
 
@@ -73,7 +80,7 @@ export default class App extends Component {
               </h5>
               <InputForm callBackFromParent={this.onFormSubmitted} />
               <TransactionList
-                transactionsToShow={this.state.transactionList}
+                transactionsToShow={this.state.transactionList.slice(0, 5)}
                 deleteTransaction={this.deleteTransaction}
               />
             </Col>
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,45 +1,79 @@
-import React, { Component } from "react";
-import { ReactComponent as DeleteImg } from "../Images/delete.svg";
+import React, {
+  Component
+} from "react";
+import {
+  ReactComponent as DeleteImg
+} from "../Images/delete.svg";
 
 export default class TransactionList extends Component {
   renderTransactionList = () => {
     let previousMonth = "";
-    return this.props.transactionsToShow.map((transaction, i) => {
-      console.log(transaction.date);
-      let formattedDate = transaction.date.toLocaleDateString(
-        "en-US",
-        { month: "long", day: "numeric", year: "numeric" }
+    let transactionsToShow = this.props.transactionsToShow.sort((a, b) => {
+      if (a.date < b.date) return -1;
+      if (a.date > b.date) return 1;
+      return 0;
+    });
+    return transactionsToShow.map((transaction, i) => {
+      let formattedDate = (new Date(transaction.date)).toLocaleDateString(
+        "en-US", {
+          month: "long",
+          day: "numeric",
+          year: "numeric"
+        }
       );
       let currentMonth = formattedDate.split(" ");
       if (currentMonth[0] !== previousMonth) {
         previousMonth = currentMonth[0];
-        return (
-          <li key={i}>
-            <h3>{currentMonth[0]}</h3>
-            <DeleteImg
-              onClick={() => this.props.deleteTransaction(transaction)}
-            />
-            Transferred {transaction.amount} on {formattedDate}
-          </li>
+        return ( <
+          li key = {
+            i
+          } >
+          <
+          h3 > {
+            currentMonth[0]
+          } < /h3> <
+          DeleteImg onClick = {
+            () => this.props.deleteTransaction(transaction)
+          }
+          />
+          Transferred {
+            transaction.amount
+          }
+          on {
+            formattedDate
+          } <
+          /li>
         );
       } else {
-        return (
-          <li key={i}>
-            <DeleteImg
-              onClick={event => this.props.deleteTransaction(transaction)}
-            />
-            Transferred {transaction.amount} on {formattedDate}
-          </li>
+        return ( <
+          li key = {
+            i
+          } >
+          <
+          DeleteImg onClick = {
+            event => this.props.deleteTransaction(transaction)
+          }
+          />
+          Transferred {
+            transaction.amount
+          }
+          on {
+            formattedDate
+          } <
+          /li>
         );
       }
     });
   };
 
   render() {
-    return (
-      <React.Fragment>
-        <ul>{this.renderTransactionList()}</ul>
-      </React.Fragment>
+    return ( <
+      React.Fragment >
+      <
+      ul > {
+        this.renderTransactionList()
+      } < /ul> <
+      /React.Fragment>
     );
   }
 }
